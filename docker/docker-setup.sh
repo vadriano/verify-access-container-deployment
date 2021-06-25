1457b33cbe2b334908191f5ff8f44edfbd8bfddb
Jon Harry <jonharry@uk.ibm.com>
2021-06-25 17:24:21 +0100

    Remove auto reload and service references.

--- docker/docker-setup.sh
+++ docker/docker-setup.sh
@@ -29,10 +29,10 @@ docker run -t -d --restart always -v pgdata:/var/lib/postgresql/data -v ${DOCKER
 
 docker run -t -d --restart always -v libldap:/var/lib/ldap -v ldapslapd:/etc/ldap/slapd.d -v libsecauthority:/var/lib/ldap.secAuthority -v ${DOCKERKEYS}/openldap:/container/service/slapd/assets/certs --hostname openldap --name openldap -e LDAP_DOMAIN=ibm.com -e LDAP_ADMIN_PASSWORD=Passw0rd -e LDAP_CONFIG_PASSWORD=Passw0rd -p ${MY_LMI_IP}:1636:636 --network isva ibmcom/verify-access-openldap:${LDAP_VERSION} --copy-service
 
-docker run -t -d --restart always -v isvaconfig:/var/shared --hostname isvaconfig --name isvaconfig -e CONTAINER_TIMEZONE=Europe/London -e ADMIN_PWD=Passw0rd -p ${MY_LMI_IP}:443:9443 -e SERVICE=config --network isva ibmcom/verify-access:${ISVA_VERSION}
+docker run -t -d --restart always -v isvaconfig:/var/shared --hostname isvaconfig --name isvaconfig -e CONTAINER_TIMEZONE=Europe/London -e ADMIN_PWD=Passw0rd -p ${MY_LMI_IP}:443:9443 --network isva ibmcom/verify-access:${ISVA_VERSION}
 
-docker run -t -d --restart always -v isvaconfig:/var/shared --hostname isvawrprp1 --name isvawrprp1 -e CONTAINER_TIMEZONE=Europe/London -p ${MY_WEB1_IP}:443:9443 -e SERVICE=webseal -e INSTANCE=rp1 -e AUTO_RELOAD_FREQUENCY=5 --network isva ibmcom/verify-access-wrp:${ISVA_VERSION}
+docker run -t -d --restart always -v isvaconfig:/var/shared --hostname isvawrprp1 --name isvawrprp1 -e CONTAINER_TIMEZONE=Europe/London -p ${MY_WEB1_IP}:443:9443 -e INSTANCE=rp1 --network isva ibmcom/verify-access-wrp:${ISVA_VERSION}
 
-docker run -t -d --restart always -v isvaconfig:/var/shared --hostname isvaruntime --name isvaruntime -e CONTAINER_TIMEZONE=Europe/London -e SERVICE=runtime -e AUTO_RELOAD_FREQUENCY=5 --network isva ibmcom/verify-access-runtime:${ISVA_VERSION}
+docker run -t -d --restart always -v isvaconfig:/var/shared --hostname isvaruntime --name isvaruntime -e CONTAINER_TIMEZONE=Europe/London --network isva ibmcom/verify-access-runtime:${ISVA_VERSION}
 
-docker run -t -d --restart always -v isvaconfig:/var/shared --hostname isvadsc --name isvadsc -e CONTAINER_TIMEZONE=Europe/London -e SERVICE=dsc -e INSTANCE=1 -e AUTO_RELOAD_FREQUENCY=5 --network isva ibmcom/verify-access-dsc:${ISVA_VERSION}
+docker run -t -d --restart always -v isvaconfig:/var/shared --hostname isvadsc --name isvadsc -e CONTAINER_TIMEZONE=Europe/London -e INSTANCE=1 --network isva ibmcom/verify-access-dsc:${ISVA_VERSION}
