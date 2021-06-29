d40ec8651b97d316e1bd8304b6949edd0972df51
Jon Harry <jonharry@uk.ibm.com>
2021-06-29 10:08:00 +0100

    OpenShift rework

--- openshift/setup-security.sh
+++ openshift/setup-security.sh
@@ -1,11 +1,13 @@
-echo "Creating Service Accounts"
-oc create serviceaccount verifyaccess
-oc create serviceaccount openldap
+echo "Creating Service Account"
+oc create serviceaccount verifyaccess-config
+oc create serviceaccount verifyaccess-anyuid
+oc create serviceaccount verifyaccess-nonroot
 
-echo "Creating isva Security Context Constraint"
+echo "Creating verifyaccess Security Context Constraint"
 oc create -f isva-security-constraint.yaml
 
 echo "Adding service accounts to Security Constraints"
-oc adm policy add-scc-to-user verifyaccess -z verifyaccess
-oc adm policy add-scc-to-user anyuid -z openldap
+oc adm policy add-scc-to-user verifyaccess -z verifyaccess-config
+oc adm policy add-scc-to-user anyuid -z verifyaccess-anyuid
+oc adm policy add-scc-to-user nonroot -z verifyaccess-nonroot
 echo "Done."
